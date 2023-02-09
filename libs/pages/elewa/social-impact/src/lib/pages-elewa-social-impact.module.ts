96df550f9de8cd67f840d9af77ef31b14cdb4ed7
Allan Ngugi <105485948+Allan-Ngugi@users.noreply.github.com>
2023-02-09 13:05:48 +0300

    ✨feat:created the neccessary component for the module

--- libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SocialImpactPageComponent } from './pages/social-impact-page/social-impact-page.component';
+
 @NgModule({
   imports: [CommonModule],
+  declarations: [SocialImpactPageComponent],
+  exports: [SocialImpactPageComponent]
 })
-export class PagesElewaSocialImpactModule {}
+export class SocialImpactModule {}
